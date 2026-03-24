efd14a82c4d3b65febbbc23763cee953ac9d9743
SalikNikodem <saliknikodem@gmail.com>
2026-03-24 15:47:08 +0100

    added high card, royal flush and straight flush

--- best_hand_search.py
+++ best_hand_search.py
@@ -1,9 +1,4 @@
-import random
-from time import process_time_ns
-
 from objects import Deck, Hand, Board, Card
-
-random.seed(2)
 
 poker_hands = ['High Card', 'Pair', 'Double Pair', 'Three of a kind', 'Straight', 'Flush', 'Full House',
                'Four of a kind', 'Straight Flush', 'Royal Flush']
@@ -28,17 +23,17 @@ reka_ustawiona = Hand(cards=[
 Card("5", '♠'),
 Card("4", '♠'),
 Card("A", '♠'),
-Card("3", '♠'),
-Card("2", '♠'),
+Card("10", '♠'),
+Card("J", '♠'),
 Card("Q", '♠'),
-Card("5", '♠')])
+Card("K", '♠')])
 reka_ustawiona2 = Hand(cards=[
 Card("Q", '♠'),
-Card("J", '♠'),
-Card("8", '♠'),
 Card("5", '♠'),
+Card("4", '♠'),
+Card("3", '♠'),
 Card("2", '♠'),
-Card("10", '♠'),
+Card("6", '♠'),
 Card("10", '♠')])
 
 rank_values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7,
@@ -94,7 +89,6 @@ def is_two_pair(hand):
         return Hand(player=hand.player, cards=final_cards)
     return None
 
-# 2 times 3 is full house, but we don't care since we are going to evaluate from the highest possible hand
 def is_three_of_a_kind(hand):
     ranks = hand.get_rank_counts_list()
     rank = None
@@ -138,13 +132,13 @@ def is_full_house(hand):
     trip_rank = None
     pair_rank = None
 
-    for rank in ranks:
+    for rank in sorted_ranks:
         if ranks[rank] >= 3:
             trip_rank = rank
             break
 
     if trip_rank:
-        for rank in ranks:
+        for rank in sorted_ranks:
             if ranks[rank] >=2 and rank != trip_rank:
                 pair_rank = rank
                 break
@@ -171,7 +165,6 @@ def is_straight(hand):
     ranks = hand.get_rank_counts_list()
 
     hand_u = hand_unique(hand)
-    # hand_u.append()
 
     sorted_ranks = sorted(ranks.keys(), key=lambda r : rank_values[r], reverse=True)
     if 'A' in sorted_ranks: sorted_ranks.append('A')
@@ -185,13 +178,28 @@ def is_straight(hand):
         if sum == 4:
             flag = (True if '5' in five and 'A' in five else False)
             if flag:
-                print("ture")
                 hand_u.ace_in_low_straight()
-
             cards = [card for card in hand_u if card._rank in five]
             return Hand(player=hand.player, cards=cards, sort=False)
     return None
 
 
-a = (is_straight(reka_ustawiona))
-a.printCards()
+def is_straight_flush(hand):
+    flush = is_flush(hand)
+    if flush:
+        straight = is_straight(flush)
+        if straight:
+            return straight
+    return None
+
+def is_royal_flush(hand):
+    straight_flush = is_straight_flush(hand)
+
+    if straight_flush:
+        if 'A' and '10' in straight_flush:
+            return straight_flush
+    return None
+
+def is_high_card(hand):
+    return Hand(player=hand.player, cards=hand[:5])
+
