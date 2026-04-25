63c2abe1a1091bc0e9936bc78070aa0bfd01a1c5
SalikNikodem <saliknikodem@gmail.com>
2026-04-25 16:16:42 +0200

    added hand statistics

--- best_hand_search.py
+++ best_hand_search.py
@@ -217,15 +217,9 @@ def game(num_of_players):
     for i in range(2):
         for hand in hands:
             hand.take_card()
-
-    for hand in hands:
-        hand.printCards()
-
     board.flop()
     board.river_and_turn()
     board.river_and_turn()
-
-    board.printCards()
 
     all_hands = []
 
@@ -241,7 +235,6 @@ def game(num_of_players):
             'name': hand_name
         })
 
-    for h in all_hands: print(h['best_5'])
 
     max_power = max(s['power'] for s in all_hands)
 
@@ -264,18 +257,55 @@ def game(num_of_players):
             elif challenger['best_5'] == current_best['best_5']:
                 winners.append(challenger)
 
-    if len(winners) > 1:
-        print(f"--- SPLIT ({len(winners)} players) ---")
+    return all_hands, winners
 
-    else:
-        print(f"--- WINNER: {winners[0]['player']} ---")
+import json
+from pathlib import Path
 
-    print("WINNER OR WINNERS")
-    for w in winners:
-        print(w['best_5'])
-        print(w['name'])
+class HandManager:
+    def __init__(self, filename="stats/hand_stats.json"):
+
+        self.file_path = Path(__file__).resolve().parent / filename
+        self.file_path.parent.mkdir(parents=True, exist_ok=True)
+        self.stats = self._load_stats()
+
+    def _load_stats(self):
+        if not self.file_path.exists():
+            return {}
+
+        try:
+            with open(self.file_path, 'r', encoding="utf-8") as ff:
+                return json.load(ff)
+        except (json.JSONDecodeError, Exception):
+            return {}
+
+    def update_stats(self, all_hands, winners):
+        num_winners = len(winners)
+        win_value = 1.0 / num_winners
+
+        for res in all_hands:
+            name = res['name']
+            if name not in self.stats:
+                self.stats[name] = {"occurrences": 0, "wins": 0.0, "splits": 0}
+
+            self.stats[name]["occurrences"] += 1
+
+        for w in winners:
+            name = w['name']
+            self.stats[name]['wins'] += win_value
+            if num_winners > 1:
+                self.stats[name]["splits"] += 1
+
+    def save_to_json(self):
+        with open(self.file_path, 'w', encoding='utf-8') as ff:
+            json.dump(self.stats, ff, indent=4, ensure_ascii=False)
 
 
+hand_stats = HandManager()
 
+for _ in range(100000):
+    all_hands, winners = game(4)
 
-game(4)
+    hand_stats.update_stats(all_hands, winners)
+
+hand_stats.save_to_json()
